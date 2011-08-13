72d06a18c0c015da882c22f727a042df5ba5c00f
Sergey Vasilyev <nolar@nolar.info>
2011-08-12 20:31:38 -0700

    Shorten and resolve actions to work with the fake url storage.

--- lib/shortener.py
+++ lib/shortener.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 from .generator import Generator
+from .datalayer import UrlStorage, UrlItem
+from .datalayer import StorageItemExists, StorageItemAbsent
+import datetime
 
 class ShortenerIdAbsentError(Exception): pass
 class ShortenerIdExistsError(Exception): pass
@@ -9,37 +12,55 @@ class Shortener(object):
     Methods for API.
     """
     
-    #!!!TODO: manual debug only
-    database = {
-        'api': None,
-    }
-    
-    domain = 'helloworld'
-    
-    def __init__(self):
+    def __init__(self, host, storage):
         super(Shortener, self).__init__()
-    
-    def shorten(self, url):
         self.generator = Generator()
-        
-        #!!! the code below should be repeated N times to generate really unique ID
-        #!!! the only way to be sure it is unique, is to store and catch a violation
-        id = self.generator.generate()
-        if id in Shortener.database:
-            raise ShortenerIdExistsError("The id requested is used already.")
-        else:
-            Shortener.database[id] = url
-            return 'http://%s/%s' % (self.domain, id)
+        self.storage = storage
+        self.host = host
     
     def resolve(self, id):
-        if id in Shortener.database and Shortener.database[id] is not None:
-            return Shortener.database[id]
-        else:
-            raise ShortenerIdAbsentError("Such id does not exists.")
+        try:
+            item = self.storage.fetch_url(self.host, id)
+            #todo later: we can add checks for moderation status here, etc.
+            return item.url
+        except StorageItemAbsent, e:
+            raise ShortenerIdAbsentError("Such url does not exist.")
     
-    def get_last(self, n):
+    def shorten(self, url, id_wanted=None, retries=3, remote_addr=None, remote_port=None):
+        """
+        Shortens the url and saves it with the id requested or generated.
+        
+        In case if the id is generated and happens to exist already, this method
+        tries to generate it few more times. This is needed to avoid collisions
+        of manually requested ids with automatically generated ids, since this
+        cannot be done by the generator itself (it should not know the purpose
+        of the ids, nor if some of the ids are reserved or used explicitly).
+        """
+        
+        while retries > 0:
+            # This code will be executed until the first success or retries will expire.
+            try:
+                id = id_wanted or self.generator.generate()
+                item = UrlItem(url, create_ts=datetime.datetime.utcnow(), remote_addr=remote_addr, remote_port=remote_port)
+                self.storage.store_url(self.host, id, item)
+                ##!!!!FIXME: url pattern should be generated or specified somewhere else
+                return 'http://%s/%s' % (self.host, id)
+            
+            # We handle only the "id exists" error here, to try few more times before failing.
+            except StorageItemExists, e:
+                
+                # If we wanted to create the url with the very specific id, we cannot recover from this error.
+                if id_wanted:
+                    raise ShortenerIdExistsError("This id exists already, try another one.")
+                
+                # If we cannot save the item for too much tries, there is something wrong with the storage itself.
+                retries -= 1
+                if retries == 0:
+                    raise e
+    
+    def get_last_urls(self, n):
         raise NotImplemented()
     
-    def get_popular(self, td):
+    def get_top_domains(self, td):
         raise NotImplemented()
 
--- v1/views.py
+++ v1/views.py
@@ -2,38 +2,49 @@
 import datetime
 from shortener.decorators import as_json, as_html, as_redirector
 from shortener.lib.shortener import Shortener
+from shortener.lib.datalayer import FakeUrlStorage
 
+def make_shortener(request):
+    return Shortener(host=request.META.get('HTTP_HOST'), #!!! or DEFAULT_HOST
+                     storage=FakeUrlStorage())
 
 @as_json
 @as_html('resolve.html')
 @as_redirector('url')
 def resolve_view(request, id):
+    shortener = make_shortener(request)
     return {
-        'url': Shortener().resolve(id)
+        'url': shortener.resolve(id)
     }
 
 @as_json
 @as_html('shorten.html')
 def shorten_view(request):
+    shortener = make_shortener(request)
     long_url = request.GET.get('url', None)
-    short_url = Shortener().shorten(long_url)
+    short_url = shortener.shorten(long_url,
+                                    remote_addr=request.META.get('REMOTE_ADDR'),
+                                    remote_port=request.META.get('REMOTE_PORT'))
     return {
         'long_url': long_url,
         'short_url': short_url,
+        'xxx': shortener.storage.__class__.data.keys(),
     }
 
 @as_json
 @as_html('last_urls.html')
 def last_urls_view(request, n):
+    shortener = make_shortener(request)
     return {
         'number': int(n),
-        'urls': Shortener().get_last(int(n)),
+        'urls': shortener.get_last_urls(int(n)),
     }
 
 @as_json
 @as_html('top_domains.html')
 def top_domains_view(request):
+    shortener = make_shortener(request)
     return {
         'days': 30,
-        'domains': Shortener().get_top(datetime.timedelta(days=30)),
+        'domains': shortener.get_top_domains(datetime.timedelta(days=30)),
     }
