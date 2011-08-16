e7c736bf57525cab86bd1d0c7d2672dcd3f95c83
Sergey Vasilyev <nolar@nolar.info>
2011-08-15 17:53:11 -0700

    URL class simplified.

--- lib/url.py
+++ lib/url.py
@@ -3,23 +3,22 @@
 class URL(dict):
     """
     Represent an URL item being handled in the system. It is not able to restore
-    its state and has no access to any storages, etc, so as it does not know how
+    its state and has no any access to storages, etc, so as it does not know how
     the data are really stored. It is just an object to organize data fields and
-    access them in a "object.field" notation.
+    access them in a "object.field" notation. It inherits from dict to be easily
+    used with JSON serializers, etc -- to mimic the built-in type.
     """
     
     def __init__(self, host, id, url, created_ts=None, remote_addr=None, remote_port=None, **kwargs):
         super(URL, self).__init__()
-        self.host = host
-        self.id = id
-        self.url = url
-        self.created_ts = float(created_ts) if created_ts is not None else created_ts
-        self.remote_addr = remote_addr
-        self.remote_port = remote_port
+        self['host'] = host
+        self['id'  ] = id
+        self['url' ] = url
+        self['created_ts' ] = float(created_ts) if created_ts is not None else created_ts
+        self['remote_addr'] = remote_addr
+        self['remote_port'] = remote_port
+        self['shortcut'] = 'http://%s/%s' % (host, id)
         #??? optionally, if something left in kwargs, probably this is an error/warning?
-        self.update(self.__dict__)
-        self.update(shortcut=self.shortcut)#!!! overrides the property, FIXME
-        #??? add generated shirtcut here too?
     
     def __str__(self):
         return self.shortcut
@@ -30,6 +29,8 @@ class URL(dict):
     def __iter__(self):
         return iter(self.__dict__.items())
     
-    @property
-    def shortcut(self):
-        return 'http://%s/%s' % (self.host, self.id)
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("URL instance has no attribute '%s'." % name)
